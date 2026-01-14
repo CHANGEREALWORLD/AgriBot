9bf4319788c9dea1e7ccf4c989c4f2a342e85ad4
unknown <l79342684@gmail.com>
2026-01-14 21:53:45 +0530

    Initial commit

--- yield_backend/main.py
+++ yield_backend/main.py
@@ -1,93 +1,3 @@
-# from fastapi import FastAPI
-# from app.routers.yield_router import router as yield_router
-
-# app = FastAPI()
-# app.include_router(yield_router)
-
-
-
-
-# from fastapi import FastAPI
-# from app.routers.yield_router import router as yield_router
-
-# app = FastAPI(
-#     title="AgriBot Backend"
-# )
-
-# # register router
-# app.include_router(yield_router)
-
-
-
-# from fastapi import FastAPI
-# from app.routers import yield_router
-# from app.routers import auth, disease, pest, soil, yield_prediction, market, weather, forum
-
-# app = FastAPI(title="AgriBot Backend")
-
-# app.include_router(auth.router)
-# app.include_router(disease.router)
-# app.include_router(pest.router)
-# app.include_router(soil.router)
-# app.include_router(yield_router.router)
-# app.include_router(market.router)
-# app.include_router(weather.router)
-# app.include_router(forum.router)
-
-
-# from fastapi import FastAPI
-# from fastapi.middleware.cors import CORSMiddleware
-
-# # Correct way to import router
-# from app.routers.yield_router import router as yield_router
-
-# app = FastAPI(title="AgriBot Backend")
-
-# # CORS (required for React -> FastAPI)
-# app.add_middleware(
-#     CORSMiddleware,
-#     allow_origins=["http://localhost:3000",
-#                     "http://127.0.0.1:3000",
-#                    ],
-#     allow_credentials=True,
-#     allow_methods=["*"],
-#     allow_headers=["*"],
-# )
-
-# # Register router
-# app.include_router(yield_router)
-
-
-
-# from fastapi import FastAPI
-# from fastapi.middleware.cors import CORSMiddleware
-# from app.routers.yield_router import router as yield_router
-# from app.routers import crop_router
-# from app.auth.google_router import router as google_router
-# from app.auth.profile import router as profile_router
-# # from app.routers import crop_router
-
-
-
-# app = FastAPI(title="AgriBot Backend")
-
-# app.add_middleware(
-#     CORSMiddleware,
-#     allow_origins=[
-#         "http://localhost:3000",
-#         "http://127.0.0.1:3000"
-#     ],
-#     allow_credentials=True,
-#     allow_methods=["*"],
-#     allow_headers=["*"],
-# )
-
-# app.include_router(yield_router)
-# app.include_router(google_router)
-# app.include_router(profile_router)
-
-
-
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 # from fastapi.staticfiles import StaticFiles
@@ -105,16 +15,24 @@ from app.routers.dashboard_router import router as dashboard_router
 app = FastAPI(title="AgriBot Backend")
 
 # CORS for React frontend
-app.add_middleware(
+# app.add_middleware(
+#     CORSMiddleware,
+#     allow_origins=[
+#         "http://localhost:3000",
+#         "http://127.0.0.1:3000",
+#     ],
+#     allow_credentials=True,
+#     allow_methods=["*"],
+#     allow_headers=["*"],
+app.add_middleware( 
     CORSMiddleware,
-    allow_origins=[
-        "http://localhost:3000",
-        "http://127.0.0.1:3000",
-    ],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
-)
+    )
+    
+
 
 # Register routers
 app.include_router(yield_router)
