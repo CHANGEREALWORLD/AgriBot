ebd6d08592ab46f1a2833bab744f79a5b6a4d68a
unknown <l79342684@gmail.com>
2026-01-14 23:57:08 +0530

    Add main.py

--- yield_backend/main.py
+++ yield_backend/main.py
@@ -1,3 +1,4 @@
+import os
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 # from fastapi.staticfiles import StaticFiles
@@ -32,7 +33,9 @@ app.add_middleware(
     allow_headers=["*"],
     )
     
-
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8000))
+    app.run(host="0.0.0.0", port=port)
 
 # Register routers
 app.include_router(yield_router)
